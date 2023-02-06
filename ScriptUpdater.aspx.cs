b7c725bd0c1c8bdf2e8b6854569b36705a1fcab7
Neeraj Tirpathi <neerajmanitripathi@gmail.com>
2023-02-06 23:33:02 +0530

    Update ScriptUpdater.aspx.cs

--- ScriptUpdater.aspx.cs
+++ ScriptUpdater.aspx.cs
@@ -8,7 +8,6 @@ namespace ScriptUpdater
 {
     public partial class Default : System.Web.UI.Page
     {
-        protected FileUpload scriptFileUpload;
         protected void browseButton_Click(object sender, EventArgs e)
         {
             if (scriptFileUpload.HasFile)
@@ -23,6 +22,7 @@ namespace ScriptUpdater
             if (File.Exists(scriptFileName))
             {
                 ScriptUpdater(scriptFileName);
+                ClearTempFolder();
             }
         }
 
@@ -45,12 +45,13 @@ namespace ScriptUpdater
                         {
                             string[] parts = line.Split(new string[] { "Text=" }, StringSplitOptions.None);
                             string textCheck = parts[1].Substring(0, parts[1].IndexOf(","));
-                            line = "web_reg_find("Text=" + textCheck + newValue1 + transactionName + "",LAST);";
+                            line = "web_reg_find(\"Text=" + textCheck + newValue1 + transactionName + "\", LAST);";
+
                         }
                     }
                     if (line.Contains("lr_start_transaction"))
                     {
-                        string[] parts = line.Split(new string[] { """ }, StringSplitOptions.None);
+                        string[] parts = line.Split(new string[] { "\"" }, StringSplitOptions.None);
                         transactionName = parts[1];
                     }
                     if (line.Contains("lr_end_transaction"))
@@ -63,6 +64,15 @@ namespace ScriptUpdater
                 sr.Close();
 
                 File.WriteAllText(scriptFileName, updatedScript.ToString());
+            }
+        }
+
+        private void ClearTempFolder()
+        {
+            DirectoryInfo di = new DirectoryInfo(Server.MapPath("~/temp/"));
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
             }
         }
     }
